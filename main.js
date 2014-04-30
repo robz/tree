73a158493c35135f3edcc1cd2544975fbb7c287e
robz <geomasta21@aol.com>
2014-04-30 00:12:14 -0500

    broke tree out to separate class

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function () {
-  var scene, camera, renderer, controls;
+  var scene, camera, renderer, controls, trees = [];
   
   function init() {
     scene = new THREE.Scene();
@@ -45,83 +45,6 @@
     ]
     .forEach(function (e) { scene.add(e.mesh); });
 
-    var hsteps = 10;
-    var taper = .6;
-    
-    var base = GRAF.makeBranch(
-      GRAF.Cylinder, 
-      {
-        br: 10, 
-        tr:10*taper, 
-        steps: hsteps,
-        h:80, 
-        parent:{mesh:scene}, 
-        shadow:true, 
-        color: 0x483113
-      }
-    );
-
-    var leaves = [];
-    leaves.push(base);
-
-    function addLeaves() {
-      var len = leaves.length;
-      
-      for (var j = 0; j < len; j++) {
-        var leaf = leaves.shift();
-        
-        for (var i = 0; i < 2; i++) { 
-          var branch = GRAF.makeBranch(
-            GRAF.Cylinder,
-            {
-              br: leaf.tr,
-              tr: leaf.tr*taper,
-              h: leaf.h*.66,
-              ry: Math.PI*2*Math.random(), 
-              rz: Math.PI/6*(i+1)*Math.random(), 
-              color: 0x483113, // brown
-              shadow: true,
-              parent: leaf
-            }
-          );
-          
-          var m = branch.mesh;
-          m.scale.x = m.scale.y = m.scale.z = .001;
-          
-          leaves.push(branch);
-        }
-      }
-    } 
-
-    var itr = 7;
-    var itrTime = 50;
-
-    setTimeout(function f() {
-      if (itr) { 
-        itr -= 1; 
-
-        var hsteps_i = 0;
-
-        setTimeout(function g() {
-          hsteps_i += 1;
-          
-          leaves.forEach(function (e) {
-            var m = e.mesh;
-            var s = hsteps_i/hsteps;
-            m.scale.x = m.scale.y = m.scale.z = s;
-            e.setTransform();
-          });
-
-          if (hsteps_i < hsteps) {
-            setTimeout(g, itrTime);
-          } else {     
-            addLeaves();
-            setTimeout(f, 0);
-          }
-        }, 0);
-      }
-    }, itrTime);
-
     var bottom = new GRAF.Box({w: 500, h: 3, d: 500, y:-3, color:0x00ff00});
     bottom.mesh.receiveShadow = false;
     scene.add(bottom.mesh);
@@ -129,7 +52,22 @@
     controls = new THREE.OrbitControls(camera);
     controls.update();
     controls.pan(0, HEIGHT/5);
+
+    for (var i = 0; i < 10; i++) { 
+      trees.push(TREE.makeTree(
+        {mesh:scene},
+        Math.random()*200 - 100, 
+        Math.random()*200 - 100
+      ));
+    }
   }
+
+
+  setTimeout(function f() {
+    trees.forEach(function (e) { e.grow(); });
+
+    setTimeout(f, 100);
+  }, 100);
 
   function animate() {
     requestAnimationFrame(animate);
---
+++ tree.js
@@ -0,0 +1,123 @@
+var TREE = {};
+
+TREE.makeTree = function (parent, x, z) {
+  var that = {};
+
+  x = x || 0;
+  z = z || 0;
+  
+  var widthDecays = [.6, .5];
+  var heightDecays = [.7, .6];
+  var zAngleDifs = [.3, 1.5];
+  var levels = 6;
+  var junctionSize = 2;
+  var numSteps = Math.floor(20+20*Math.random());
+  var initialHeight = 80;
+  
+  var base = GRAF.makeBranch(
+    GRAF.Cylinder, 
+    {
+      x: x,
+      z: z,
+      br: 10, 
+      tr: 10 * widthDecays[0],
+      steps: numSteps,
+      h: initialHeight, 
+      parent: parent, 
+      shadow: true, 
+      color: 0x483113
+    }
+  );
+          
+  var m = base.mesh;
+  m.scale.x = m.scale.y = m.scale.z = .001;
+          
+
+  var tips = [];
+  tips.push(base);
+
+  function addTips(areLeaves) {
+    var len = tips.length;
+    
+    for (var j = 0; j < len; j++) {
+      var tip = tips.shift();
+     
+      if (areLeaves) {
+        var leaf = GRAF.makeBranch(
+          GRAF.Sphere,
+          {
+            r: 10,
+            ry: 0,
+            rz: 0,
+            color: 0x00ff00,
+            shadow: true,
+            parent: tip
+          }
+        );
+
+        var m = leaf.mesh;
+        m.scale.x = m.scale.y = m.scale.z = .001;
+        
+        tips.push(leaf);
+      } else { 
+        for (var i = 0; i < junctionSize; i++) { 
+          var branch = GRAF.makeBranch(
+            GRAF.Cylinder,
+            {
+              br: tip.tr,
+              tr: tip.tr * widthDecays[i],
+              h: tip.h * heightDecays[i],
+              ry: Math.PI*2*Math.random(), 
+              rz: zAngleDifs[i]*Math.random(), 
+              color: 0x483113, // brown
+              shadow: true,
+              parent: tip
+            }
+          );
+          
+          var m = branch.mesh;
+          m.scale.x = m.scale.y = m.scale.z = .001;
+          
+          tips.push(branch);
+        }
+      }
+    }
+  } 
+
+  // returns whether it is done growing
+  that.grow = (function () {
+    var itr = levels;
+    var innerSteps = 0;
+    var doneGrowing = false;
+
+    return function() {
+      if (doneGrowing) {
+        return true;
+      }  
+
+      if (innerSteps === numSteps) {
+        itr -= 1;
+      
+        if (itr == 0) {
+          doneGrowing = true;
+          return true;
+        }
+        
+        addTips(itr === 1);
+        innerSteps = 0;
+      }
+      
+      innerSteps += 1;
+
+      tips.forEach(function (e) {
+        var m = e.mesh;
+        m.scale.x = m.scale.y = m.scale.z = innerSteps / numSteps;
+        e.setTransform();
+      });
+
+      return false;
+    };
+  }());
+
+  return that;
+};
